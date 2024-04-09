eac335daad57bf322fbc3bea618961654052b95e
pedrosimiao <pedrolumiere@gmail.com>
2024-04-09 08:07:49 -0300

    estilizando a sidebar

--- src/components/Paragrafo/index.tsx
+++ src/components/Paragrafo/index.tsx
@@ -2,11 +2,14 @@ import { P } from './styles'
 
 export type Props = {
   children: string
+  fontSize?: number
   tipo?: 'principal' | 'secundario'
 }
 
-const Paragrafo = ({ children, tipo = 'principal' }: Props) => (
-  <P tipo={tipo}>{children}</P>
+const Paragrafo = ({ children, fontSize, tipo = 'principal' }: Props) => (
+  <P fontSize={fontSize} tipo={tipo}>
+    {children}
+  </P>
 )
 
 export default Paragrafo
